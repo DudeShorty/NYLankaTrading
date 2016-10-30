ba0ba72afce614554ead32c6b87e16913f5c7bdc
stelan <stelan@sp-solution.biz>
2016-10-30 20:49:40 +0530

    brand

--- src/main/java/com/incosyz/service/ItemService.java
+++ src/main/java/com/incosyz/service/ItemService.java
@@ -7,5 +7,5 @@ import com.incosyz.entity.Item;
  * Created by Stelan Briyan on 10/30/2016.
  */
 public interface ItemService {
-    public void add(ItemDTO itemDTO);
+    void add(ItemDTO itemDTO);
 }
