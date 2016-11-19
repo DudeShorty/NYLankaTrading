f23ef869548dd7f58ed21b7b4030b88bf4e4cc6e
stelan <stelan@sp-solution.biz>
2016-11-20 00:00:18 +0530

    Changes

--- src/main/java/com/incosyz/service/impl/ItemTankServiceImpl.java
+++ src/main/java/com/incosyz/service/impl/ItemTankServiceImpl.java
@@ -1,6 +1,12 @@
 package com.incosyz.service.impl;
 
 import com.incosyz.dao.AbstractDao;
+import com.incosyz.dao.ItemDao;
+import com.incosyz.dao.TankDao;
+import com.incosyz.dto.ItemTankDTO;
+import com.incosyz.entity.Item;
+import com.incosyz.entity.ItemTank;
+import com.incosyz.entity.Tank;
 import com.incosyz.service.ItemService;
 import com.incosyz.service.ItemTankService;
 import com.incosyz.service.TankService;
@@ -16,9 +22,36 @@ public class ItemTankServiceImpl implements ItemTankService {
     private AbstractDao abstractDao;
 
     @Autowired
-    private TankService tankService;
+    private TankDao tankDao;
 
     @Autowired
-    private ItemService itemService;
+    private ItemDao itemDao;
 
+    public void save(ItemTankDTO itemTankDTO) {
+        ItemTank itemTank = generateItenTankDto(itemTankDTO);
+        if (itemTank != null) {
+            abstractDao.save(itemTank);
+        }
+    }
+
+    public ItemTank generateItenTankDto(ItemTankDTO itemTankDTO) {
+        ItemTank itemTank = new ItemTank();
+
+        Long itemId = itemTankDTO.getItemId();
+        if (itemId != null) {
+            Item item = itemDao.getItem(itemId);
+            itemTank.setItem(item);
+        }
+
+        Long tankId = itemTankDTO.getTankId();
+        if (tankId != null) {
+            Tank tank = tankDao.getTanks(tankId);
+            itemTank.setTank(tank);
+        }
+
+        itemTank.setQty(itemTankDTO.getQty());
+        itemTank.setUnitPrice(itemTankDTO.getUnitPrice());
+
+        return itemTank;
+    }
 }
