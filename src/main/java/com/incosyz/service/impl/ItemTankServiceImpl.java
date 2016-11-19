96ab216c022c3325cadcb25db5d8ab9ebc95e089
stelan <stelan@sp-solution.biz>
2016-11-19 22:35:10 +0530

    Item Tank Service Added

---
+++ src/main/java/com/incosyz/service/impl/ItemTankServiceImpl.java
@@ -0,0 +1,12 @@
+package com.incosyz.service.impl;
+
+import com.incosyz.service.ItemService;
+import com.incosyz.service.ItemTankService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Stelan Briyan on 11/19/2016.
+ */
+@Service
+public class ItemTankServiceImpl implements ItemTankService {
+}
