db9f8a37ed259f942797ab7a3c06775ad52c1ca6
Manoj Hewage <whmanoj@gmail.com>
2016-11-21 22:48:39 +0530

    complete login

--- src/js/controllers/item-tank-controller.js
+++ src/js/controllers/item-tank-controller.js
@@ -3,81 +3,90 @@
  */
 
 angular.module('RDash')
-    .controller('ItemTankCtrl', ['$scope', 'ItemService', 'TankService', 'ItemTankService', 'AlertService',
-        function ($scope, ItemService, TankService, ItemTankService, AlertService) {
+    .controller('ItemTankCtrl', ['$scope', 'ItemService', 'TankService', 'ItemTankService',
+        'AlertService', '$cookieStore', '$location',
+        function ($scope, ItemService, TankService, ItemTankService, AlertService, $cookieStore, $location) {
 
-            $scope.loadItemTankDTO = loadItemTankDTO;
-            function loadItemTankDTO() {
-                $scope.itemTankDTO = {
-                    "tank_id": null,
-                    "qty": 0,
-                    "unit_price": 0
+            if ($cookieStore.get('authenticated')) {
+                $scope.loadItemTankDTO = loadItemTankDTO;
+                function loadItemTankDTO() {
+                    $scope.itemTankDTO = {
+                        "tank_id": null,
+                        "qty": 0,
+                        "unit_price": 0
+                    }
                 }
-            }
-            loadItemTankDTO();
 
-            $scope.refreshItemTankDTO = refreshItemTankDTO;
-            function refreshItemTankDTO(tank_id) {
-                $scope.itemTankDTO = {
-                    "tank_id": tank_id,
-                    "qty": 0,
-                    "unit_price": 0
+                loadItemTankDTO();
+
+                $scope.refreshItemTankDTO = refreshItemTankDTO;
+                function refreshItemTankDTO(tank_id) {
+                    $scope.itemTankDTO = {
+                        "tank_id": tank_id,
+                        "qty": 0,
+                        "unit_price": 0
+                    }
                 }
-            }
 
-            $scope.loadItems = loadItems;
-            function loadItems() {
-                ItemService.readActive()
-                    .success(function (data) {
-                        $scope.items = data;
-                    })
-                    .error(function (error) {
-                        console.error(error);
-                    });
-            }
-            loadItems();
-
-            $scope.loadTanks = loadTanks;
-            function loadTanks() {
-                TankService.readActive()
-                    .success(function (data) {
-                        $scope.tanks = data;
-                    })
-                    .error(function (error) {
-                        console.error(error);
-                    });
-            }
-            loadTanks();
-
-            $scope.loadItemTanks = loadItemTanks;
-            function loadItemTanks() {
-                if ($scope.itemTankDTO.tank_id == null || $scope.itemTankDTO.tank_id == '') {
-                    $scope.itemTanks = [];
-                } else {
-                    ItemTankService.read($scope.itemTankDTO.tank_id)
+                $scope.loadItems = loadItems;
+                function loadItems() {
+                    ItemService.readActive()
                         .success(function (data) {
-                            $scope.itemTanks = data;
+                            $scope.items = data;
                         })
                         .error(function (error) {
                             console.error(error);
                         });
                 }
-            }
-            loadItemTanks();
 
-            $scope.addItemTank = addItemTank;
-            function addItemTank() {
-                console.log('addItemTank');
-                ItemTankService.insert($scope.itemTankDTO)
-                    .success(function () {
-                        loadItems();
-                        loadTanks();
-                        refreshItemTankDTO($scope.itemTankDTO.tank_id);
-                        loadItemTanks();
-                        AlertService.addSuccessAlert('Item Successfully Added To Tank.');
-                    })
-                    .error(function (error) {
-                        console.error(error);
-                    });
+                loadItems();
+
+                $scope.loadTanks = loadTanks;
+                function loadTanks() {
+                    TankService.readActive()
+                        .success(function (data) {
+                            $scope.tanks = data;
+                        })
+                        .error(function (error) {
+                            console.error(error);
+                        });
+                }
+
+                loadTanks();
+
+                $scope.loadItemTanks = loadItemTanks;
+                function loadItemTanks() {
+                    if ($scope.itemTankDTO.tank_id == null || $scope.itemTankDTO.tank_id == '') {
+                        $scope.itemTanks = [];
+                    } else {
+                        ItemTankService.read($scope.itemTankDTO.tank_id)
+                            .success(function (data) {
+                                $scope.itemTanks = data;
+                            })
+                            .error(function (error) {
+                                console.error(error);
+                            });
+                    }
+                }
+
+                loadItemTanks();
+
+                $scope.addItemTank = addItemTank;
+                function addItemTank() {
+                    console.log('addItemTank');
+                    ItemTankService.insert($scope.itemTankDTO)
+                        .success(function () {
+                            loadItems();
+                            loadTanks();
+                            refreshItemTankDTO($scope.itemTankDTO.tank_id);
+                            loadItemTanks();
+                            AlertService.addSuccessAlert('Item Successfully Added To Tank.');
+                        })
+                        .error(function (error) {
+                            console.error(error);
+                        });
+                }
+            } else {
+                $location.path('/login');
             }
         }]);
--- src/js/controllers/tank-controller.js
+++ src/js/controllers/tank-controller.js
@@ -5,45 +5,52 @@
  * Created by Aux-058 on 11/6/2016.
  */
 angular.module('RDash')
-    .controller('TankCtrl', ['$scope', 'TankService', 'AlertService', function ($scope, TankService, AlertService) {
+    .controller('TankCtrl', ['$scope', 'TankService', 'AlertService', '$cookieStore', '$location',
+        function ($scope, TankService, AlertService, $cookieStore, $location) {
 
-        console.log('TankCtrl');
+            if ($cookieStore.get('authenticated')) {
+                console.log('TankCtrl');
 
-        $scope.readTanks = readTanks;
-        function readTanks() {
-            TankService.read()
-                .success(function (data) {
-                    $scope.tanks = data;
-                })
-                .error(function (error) {
-                    console.error(error);
-                })
-        }
-        readTanks();
+                $scope.readTanks = readTanks;
+                function readTanks() {
+                    TankService.read()
+                        .success(function (data) {
+                            $scope.tanks = data;
+                        })
+                        .error(function (error) {
+                            console.error(error);
+                        })
+                }
 
-        $scope.refreshTankDTO = refreshTankDTO;
-        function refreshTankDTO() {
-            var date = new Date();
-            $scope.tankDTO = {
-                "name": null,
-                "identify_number": null,
-                "received_date": date,
-                "status": null
-            };
-        }
-        refreshTankDTO();
+                readTanks();
 
-        $scope.addTank = addTank;
-        function addTank() {
-            console.log('addTank');
-            TankService.insert($scope.tankDTO)
-                .success(function () {
-                    refreshTankDTO();
-                    readTanks();
-                    AlertService.addSuccessAlert('Tank Saved Successfully.')
-                })
-                .error(function (error) {
-                    console.error(error);
-                });
-        }
-    }]);
+                $scope.refreshTankDTO = refreshTankDTO;
+                function refreshTankDTO() {
+                    var date = new Date();
+                    $scope.tankDTO = {
+                        "name": null,
+                        "identify_number": null,
+                        "received_date": date,
+                        "status": null
+                    };
+                }
+
+                refreshTankDTO();
+
+                $scope.addTank = addTank;
+                function addTank() {
+                    console.log('addTank');
+                    TankService.insert($scope.tankDTO)
+                        .success(function () {
+                            refreshTankDTO();
+                            readTanks();
+                            AlertService.addSuccessAlert('Tank Saved Successfully.')
+                        })
+                        .error(function (error) {
+                            console.error(error);
+                        });
+                }
+            } else {
+                $location.path('/login');
+            }
+        }]);
